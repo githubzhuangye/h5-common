040550a8c66913d75e5b6521ec407e76d5db1797
庄叶 <zhuangshujie@bobfintech.com.cn>
2020-06-23 09:16:00 +0800

    添加demo页面

---
+++ src/page/apply/success.vue
@@ -0,0 +1,23 @@
+<!-- home -->
+<template>
+    <div class="container">提交成功</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {};
+    },
+    components: {},
+
+    computed: {},
+
+    created() {},
+    methods: {},
+};
+</script>
+<style lang="less" scoped>
+.container {
+    background-color: #dedede;
+}
+</style>
---
+++ src/page/bank/list.vue
@@ -0,0 +1,24 @@
+<!-- home -->
+<template>
+    <div class="container">支行列表</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {};
+    },
+
+    computed: {},
+
+    async created() {},
+    mounted() {},
+
+    methods: {},
+};
+</script>
+<style lang="less" scoped>
+.container {
+    background-color: #cdcdcd;
+}
+</style>
